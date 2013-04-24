bd13079f00d0b4f8b8bb6e8d8df79d93dc9cf3d9
Gendo Ikari <gendoikari.hack@gmail.com>
2013-04-24 23:28:26 +0200

    Using mgo ad mongodb driver.

--- backend/boot.go
+++ backend/boot.go
@@ -3,11 +3,15 @@ package backend
 import (
     "log"
     "./server"
+    "./data"
 )
 
 func Boot(listening_url string, db_url string) {
     log.Println("Booting backend...")
 
+    db := data.New()
     s := server.New()
+
+    db.Start(db_url)
     s.Start(listening_url)
 }
--- start.go
+++ start.go
@@ -3,5 +3,5 @@ package main
 import "./backend"
 
 func main() {
-    backend.Boot("localhost:8080", "http://localhost:9080")
+    backend.Boot("localhost:8080", "localhost:9080")
 }
