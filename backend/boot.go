95710cb321de70d68f1310575f6a42aa6c445d42
Gendo Ikari <gendoikari.hack@gmail.com>
2013-04-25 14:44:50 +0200

    Setting golang runtime options

--- backend/boot.go
+++ backend/boot.go
@@ -2,12 +2,22 @@ package backend
 
 import (
     "log"
+    "runtime"
     "./server"
     "./data/mongodb"
 )
 
 func Boot(listening_url string, db_url string) {
     log.Println("Booting backend...")
+
+    cpus := runtime.NumCPU()
+    if runtime.GOMAXPROCS(cpus) >= 1 {
+        log.Println("Using", cpus, "core/s")
+    } else {
+        log.Println("WARNING: i cannot use", cpus, "core/s")
+    }
+
+    log.Println("Server startup...")
 
     db := mongodb.New()
     s := server.New()
